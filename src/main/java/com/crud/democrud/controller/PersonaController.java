d5e2e0a0e7322333e8110a1da0e25416e3c260ce
JuanVin <67810071+JuanVin@users.noreply.github.com>
2021-08-02 21:03:42 -0300

    "add html methods"

--- src/main/java/com/crud/democrud/controller/PersonaController.java
+++ src/main/java/com/crud/democrud/controller/PersonaController.java
@@ -5,11 +5,15 @@
  */
 package com.crud.democrud.controller;
 
+import com.crud.democrud.model.Persona;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.crud.democrud.service.PersonaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 /**
  *
  * @author juan
@@ -25,4 +29,23 @@ public class PersonaController {
         return "index";
     }
     
+    @GetMapping("/save/{id}")
+    public String save(@PathVariable Long id, Model model){
+        if (id != null){
+            model.addAttribute("persona", personaService.get(id));
+        }
+        return "save";
+    }
+    
+    @PostMapping("/save/{id}")
+    public String save(Persona persona, Model model){
+        personaService.save(persona);
+        return "redirect:/";
+    }
+    
+    @PostMapping("/delete/{id}")
+    public String delete(@PathVariable Long id, Model model){
+        personaService.delete(id);
+        return "redirect:/";
+    }
 }
